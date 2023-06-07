2b2c1d772ba42f1e59d1f75806683f078bd5a55d
Renan <renansouza2310@gmail.com>
2023-06-06 22:45:24 -0300

    Tablet design done.

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ module.exports = {
   content: ["./src/*.{js,html}"],
   theme: {
     extend: {
+      backgroundImage: {
+        'tablet': "url('../assets/tablet/image-host.jpg')"
+      },
       colors: {
         'black': '#121725',
         'dark-blue': '#2C344B',
@@ -20,15 +23,24 @@ module.exports = {
         'title-line-height': '3.875rem'
       },
       maxWidth: {
-        'spotify-logo': '3.5rem',
-        'apple-logo': '2.813rem',
-        'google-logo': '4.563rem',
-        'pocket-logo': '4.813rem'
+        'sm-spotify-logo': '3.5rem',
+        'sm-apple-logo': '2.813rem',
+        'sm-google-logo': '4.563rem',
+        'sm-pocket-logo': '4.813rem',
+        'sm-main-container': '39.688rem',
+        'md-spotify-logo': '6rem',
+        'md-apple-logo': '4.875rem',
+        'md-google-logo': '7.813rem',
+        'md-pocket-logo': '8.063rem',
+        'md-main-container': '39.688rem'
       },
       maxHeight: {
-        'spotify-apple-logo': '1.063rem',
-        'google-logo': '0.688rem',
-        'pocket-logo': '0.938rem'
+        'sm-spotify-apple-logo': '1.063rem',
+        'sm-google-logo': '0.688rem',
+        'sm-pocket-logo': '0.938rem',
+        'md-spotify-apple-logo': '1.813rem',
+        'md-google-logo': '1.125rem',
+        'md-pocket-logo': '1.625rem'
       }
     },
   },

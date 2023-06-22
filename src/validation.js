48268a095879b48a34071a36a85bb53fb3ce2b8c
Renan <renansouza2310@gmail.com>
2023-06-22 09:47:43 -0300

    Adding new validation to email input when the input is empty.

--- src/validation.js
+++ src/validation.js
@@ -1,11 +1,19 @@
 function validateEmail(ev) {
   const email = ev.children.email.value
   const errorMessage = ev.children.errorMessage
+  const addEmailErrorMessage = ev.children.addEmailErrorMessage
   const matcher = /\w{2,}@[a-z]{2,}\.[a-z]{2,}/
 
-  if (!email.match(matcher)) {
-    errorMessage.classList.remove("invisible")
-  } else {
+  console.log(email)
+
+  if (email === "") {
     errorMessage.classList.add("invisible")
+    addEmailErrorMessage.classList.remove("invisible")
+  } else if (!email.match(matcher)) {
+    errorMessage.classList.remove("invisible")
+    addEmailErrorMessage.classList.add("invisible")
+  } else if (email.match(matcher)) {
+    errorMessage.classList.add("invisible")
+    addEmailErrorMessage.classList.add("invisible")
   }
 }
